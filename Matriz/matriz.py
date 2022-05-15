49f3a5c5b675523669f54d25f61f3fdcffa2d141
Ezequiel Antoine <ezequielantoine@gmail.com>
2022-05-15 14:39:11 -0300

    Commit Ejercicio 2: Matriz

---
+++ Matriz/matriz.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+
+def crearMatriz():
+
+    matriz=np.random.randint(10,size=(5,5))
+
+    return matriz      
+
+def buscarConsecutivos(matriz):
+    pass
+
+
+m=crearMatriz()
+print(m)
