7fe411561e11c0afb253f99f8d7aa4bc7faba94f
Ezequiel Antoine <ezequielantoine@gmail.com>
2022-05-15 15:40:17 -0300

    Commit Ejercicio 2: Matriz - Metodo buscarConsecutivos terminado

--- Matriz/matriz.py
+++ Matriz/matriz.py
@@ -8,8 +8,29 @@ def crearMatriz():
     return matriz      
 
 def buscarConsecutivos(matriz):
-    pass
+    
+    cont=1
+    inicio=0
+    
+    for i in range(0,5): 
+        inicio=matriz[i][0]
+        anterior=inicio
+        cont=1
+
+        for j in range(0,5):
+            if (anterior+1==matriz[i][j]): 
+                cont+=1
+                if cont==4:
+                    print("Se encontro una cadena de 4 numeros consecutivos!")
+                    print(f"Valor inicial: ",inicio)
+                    print(f"Valor final: ",matriz[i][j])
+                    break
+            else: cont=1            #Si no es igual al anterior vuelve el cont a 1.
+
+            anterior=matriz[i][j]
+
 
 
 m=crearMatriz()
 print(m)
+buscarConsecutivos(m)
